041f3d4a09a7a0af693369f6eeba31493863892a
Gleydson Albuquerque <gassantos.dev@gmail.com>
2021-06-05 20:36:37 -0300

    return the product price via SSG

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import styles from './styles.module.scss'
 
-export function SubscribeButton(){
+interface ISubscribeButtonProps {
+    priceId: string;
+}
+
+export function SubscribeButton({priceId}: ISubscribeButtonProps){
     return (
         <button
             className={styles.subscribeButton}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import styles from './home.module.scss'
 
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
+import { browser } from 'process'
+
+interface IHomeProps {
+  product: {
+    priceId: string;
+    amount: string;
+  }
+}
 
 
-export default function Home() {
+export default function Home({product}: IHomeProps) {
   return (
     <>
       <Head>
@@ -18,10 +26,10 @@ export default function Home() {
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>Get access to all the publications <br/>
-            <span>for $ 9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -30,12 +38,33 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe
-  
+// SSR = SERVER-SIDE RENDERING
+// Permite buscar dados no backend e montar a pagina HTML ainda neste backend antes 
+// de renderizar no browser
+// A vantagem é conseguimos visualizar o conteúdo da página (renderizar os dados da 
+//   api) mesmo com o javascript desativado
+
+// SSG = STATIC SITE GENERATION
+// Tem praticamente as mesmas funcionalidades do SSR, porém, seu diferencial é guardar
+// uma versão do HTML gerado e retornar este mesmo HTML sem precisar fazer uma nova 
+// consulta à API, reduzindo o custo de processamento e o tempo de resposta do backend.
+// Os dados podem ser revalidados de acordo com o prazo que definirmos no campo revalidate
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Iz7R1Fvt9vVC1qeGXNO7D6p');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format((price.unit_amount/100)),
+  }
+
   return {
     props: {
-      name: "Gleydson"
-    }
+      product
+    },
+    revalidate: 24 * 60 * 60 //24 hrs
   }
 }
